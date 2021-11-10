7ab075abf15a8d8d544bf693da544c63e3c6b68b
Georges TAVERNE <gtaverne@e2r2p4.clusters.42paris.fr>
2021-11-10 15:45:59 +0100

    On avance sur day 1

--- module01/ex03/matrix.py
+++ module01/ex03/matrix.py
@@ -2,16 +2,24 @@ from copy import deepcopy
 
 class Matrix():
 	def __init__(self, *args):
-		self.mat = []
-		self.lines = 0
-		for i in args:
-			self.lines += 1
-			self.mat.append(i)
-		self.columns = len(self.mat[0])
-		if self.lines > 1:
-			for i in self.mat[1:]:
-				if self.columns != len(i):
+		if all(isinstance(ele, list) for ele in args[0]):
+			self.mat = args[0]
+			self.lines = len(self.mat)
+			self.columns = len(self.mat[0])
+			for i in self.mat:
+				if len(i) != self.columns:
 					print("Invalid matrix, all lines don't have the same size")
+		else:
+			self.mat = []
+			self.lines = 0
+			for i in args:
+				self.lines += 1
+				self.mat.append(i)
+			self.columns = len(self.mat[0])
+			if self.lines > 1:
+				for i in self.mat[1:]:
+					if self.columns != len(i):
+						print("Invalid matrix, all lines don't have the same size")
 
 
 	def __str__(self):
@@ -20,6 +28,10 @@ class Matrix():
 			txt += str(i) + ",\n"
 		txt += str(self.mat[-1]) + "]"
 		return txt
+
+	def __repr__(self):
+		return str(self) + "\nlines: " + str(self.lines) + "  columns: "+ str(self.columns)
+
 
 	def __add__(self, other):
 		if isinstance(other, Matrix):
@@ -95,9 +107,30 @@ class Matrix():
 	def __rtruediv__(self, other):
 		print("You cannot divide by a matrix")
 # # div : only scalars.
-# __mul__
+	def __mul__(self, other):
+		if isinstance(other, Matrix):
+			if self.columns == other.lines:
+				tmp = [[0 for col in range(other.columns)] for lines in range(self.lines)]
+				for i in range(self.lines):
+					for j in range(other.columns):
+						gl = self.mat[i]
+						dc = [other.mat[x][j] for x in range(other.lines)]
+						tmp[i][j] = sum([a * b for a, b in zip(gl, dc)])
+				return (Matrix(tmp))
+
+			elif other.columns == 1 and other.lines ==1:
+				return self * other.mat[0][0]
+			print("You cannot divide by a matrix")
+			return
+		else:
+			res = deepcopy(self)
+			for i in range(res.lines):
+				res.mat[i] = [a * other for a in self.mat[i]]
+			return res
+		
+
+
 # __rmul__
 # # mul : scalars, vectors and matrices , can have errors with vectors and matrices.
 # # if we perform Matrix * Vector (dot product), return a Vector.
 # __str__
-# __repr__
--- module01/ex03/test.py
+++ module01/ex03/test.py
@@ -27,3 +27,15 @@ print("a / a", a/a, "\n")
 print("a / 5", a/5, "\n")
 print("7 / a", 7 / a, "\n")
 print("vecmat / vecmat", vecmat / vecmat, "\n")
+
+k = Matrix([[0,1,2,3], [4,5,6,7], [8,9,10,11]]) +10
+print(k)
+
+id4 = Matrix([1,0,0,0], [0,1,0,0], [0,0,1,0], [0,0,0,1])
+
+print("k * b", k * b, "\n")
+print("a * id4", a * id4, "\n")
+print("id4 * b", id4 * b, "\n")
+print("a * vecmat", a * vecmat, "\n")
+print("b * vecmat", b * vecmat, "\n")
+
